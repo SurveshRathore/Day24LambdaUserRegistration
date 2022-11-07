d44a25faac604da04a122003ac8be67d5915b5da
SurveshRathore <113424413+SurveshRathore@users.noreply.github.com>
2022-11-07 18:08:13 +0530

    UC7 to validate the user pass have a number

---
+++ UC7PassOneNumeric.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day24LambdaUserReg
+{
+    internal class UC7PassOneNumeric
+    {
+        //one character in uppercase + one number password regex
+        String nameRegex = "(?=.*[A-Z])(?=.*[0-9]).{8}";
+        public void CheckNumericInPassword()
+        {
+            
+            Console.Write("Enter the password with minimum 8 characters: ");
+            String passChar = Console.ReadLine();
+            //compare the password regex with the user entered password
+            Console.WriteLine(ValidateRegex(passChar));
+        }
+        //compare the name regex with the user entered first name using the lambda
+        public bool ValidateRegex(string userValue) => (Regex.IsMatch(userValue, nameRegex));
+    }
+}
